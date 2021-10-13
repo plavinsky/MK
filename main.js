7e01593f621ac4024e87d07aa7513ef31c57b73c
plavinsky <49480393+plavinsky@users.noreply.github.com>
2021-10-13 23:06:09 +0300

    task0

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
 console.log("Fight..");
+
+const player1 = {
+    name: 'Sonya',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/sonya.gif',
+    weapon: ['ice', 'fire'],
+    attack: function() {
+        console.log(this.name + " " + "Fight!");
+    }
+}
+
+const player2 = {
+    name: 'Scorpion',
+    hp: 100,
+    img: 'http://reactmarathon-api.herokuapp.com/assets/scorpion.gif',
+    weapon: ['hit', 'fire'],
+    attack: function() {
+        console.log(this.name + " " + "Fight!");
+    }
+}
+
+player1.attack();
+player2.attack();
